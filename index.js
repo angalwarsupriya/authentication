3305e20560b05f8fd2303f9e449062aac92dbc0a
supriya <angalwarsupriya@gmail.com>
2024-10-12 11:00:02 +0530

    initial

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { open } = require('sqlite');
+const sqlite3 = require('sqlite3');
+const app = express();
+
+app.use(express.json());
+
+const dbPath = path.join(__dirname, 'website.db');
+let db = null;
+
+const initializeServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(process.env.PORT || 3000, () => console.log('Server running!'));
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+  }
+};
+
+initializeServer();
+
+
+
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  console.log(name, email,password)
+
+  const selectUserQuery = `SELECT * FROM users WHERE name = '${name}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser) {
+    res.status(400).send('User already exists');
+ } else {
+    const createUserQuery = `INSERT INTO users (name, email, password) 
+    VALUES (
+    '${name}',
+    '${email}',
+    '${hashedPassword}'
+    )`
+    await db.run(createUserQuery);
+    res.status(201).send('Created new user');
+   
+
+ }
+  
+});
+
+app.post('/login', async (req, res) => {
+  const { name, password } = req.body;
+  const selectUserQuery = `SELECT * FROM users WHERE name = '${name}' `;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (!dbUser) {
+    res.status(400).send('Invalid user');
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const payload = { name};
+      const jwtToken = jwt.sign(payload, process.env.JWT_SECRET || 'MY_SECRET_TOKEN');
+      res.send({ jwtToken });
+    } else {
+      res.status(400).send('Invalid Password');
+    }
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Home Route');
+});
