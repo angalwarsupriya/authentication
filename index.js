58a254f7a0bce032065831edbca21fa9ed54c7df
supriya <angalwarsupriya@gmail.com>
2024-11-11 14:45:28 +0530

    updated login and profile api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
 const app = express();
@@ -24,56 +24,34 @@ const initializeServer = async () => {
 
 initializeServer();
 
-app.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
-  
-  const selectUserQuery = `SELECT * FROM users WHERE name = "${name}"`;
-  let dbUser;
-  try {
-    dbUser = await db.get(selectUserQuery);
-  } catch (err) {
-    console.error("Select user error:", err);
-    return res.status(500).send("Error while selecting user");
-  }
-
-  if (dbUser) {
-    return res.status(400).send('User already exists');
-  }
-
-  const createUserQuery = `INSERT INTO users (name, email, password) 
-  VALUES (
-  '${name}',
-  '${email}',
-  '${password}')`;
-  try {
-    await db.run(createUserQuery);
-    return res.status(201).send('Created new user');
-  } catch (err) {
-    console.error("Insert user error:", err);
-    return res.status(500).send('Internal Server Error');
-  }
-});
-
-
-/// login 
-app.post('/login', async (req, res) => {
-  const { name, password } = req.body;
-  const selectUserQuery = `SELECT * FROM users WHERE name = '${name}' `;
-  const dbUser = await db.get(selectUserQuery);
-
-  if (!dbUser) {
-    res.status(400).send('Invalid user');
-  } else {
-    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
-    if (isPasswordMatched) {
-     
-      res.send('123');
-    } else {
-      res.status(400).send('Invalid Password');
+app.get('/login', async(req, res)=>{
+  const profileDetails = req.body
+  const {name, email} = profileDetails
+  if (name==='Supriya'){
+    if(email==='supriya@gmail.com'){
+      const payload = {username:name}
+      const token = jwt.sign(payload,'TEXT')
+      res.status(200)
+      res.send({"jwt_token":token})
+    }
+    else{
+      res.status(400)
+      res.send({"error":"Username or password is invalid"})
     }
   }
+  else{
+    res.status(400)
+    res.send({"error":"Username or password is invalid"})
+  }
 });
 
-app.get('/', (req, res) => {
-  res.send('Home Route');
-});
+
+app.get('/profile', async(req, res)=>{
+  const profileQuery = `
+  SELECT * FROM users
+  WHERE name='Supriya'
+  `
+  const profilequeryRes=await db.get(profileQuery)
+  res.status(200)
+  res.send(profilequeryRes)
+})
